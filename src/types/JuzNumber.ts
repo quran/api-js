2848ff5d9e118c0292908fd7d810aa5052e5cd8e
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2022-04-06 16:37:15 +0300

    feat: optimize static types and make them dynamic

--- src/types/JuzNumber.ts
+++ src/types/JuzNumber.ts
@@ -1,31 +1,4 @@
-export type JuzNumber =
-  | '1'
-  | '2'
-  | '3'
-  | '4'
-  | '5'
-  | '6'
-  | '7'
-  | '8'
-  | '9'
-  | '10'
-  | '11'
-  | '12'
-  | '13'
-  | '14'
-  | '15'
-  | '16'
-  | '17'
-  | '18'
-  | '19'
-  | '20'
-  | '21'
-  | '22'
-  | '23'
-  | '24'
-  | '25'
-  | '26'
-  | '27'
-  | '28'
-  | '29'
-  | '30';
+import { NumberRange, NumberUnionToString } from './utils';
+
+type _JuzNumber = NumberRange<1, 31>;
+export type JuzNumber = _JuzNumber | NumberUnionToString<_JuzNumber>;
