215e85697297a1baa9ff79e31b3ca9a40c3abb99
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2023-04-10 01:58:10 +0300

    fix: don't mutate mergeApiOptions params so that fetchFn works

--- test/setup.ts
+++ test/setup.ts
@@ -1,13 +1,15 @@
 import fetch from 'cross-fetch';
-import { beforeAll, afterEach, afterAll } from 'vitest';
+import { beforeAll, afterEach, afterAll, beforeEach } from 'vitest';
 import { server } from '../mocks/server';
 
 // Establish API mocking before all tests.
 beforeAll(async () => {
   server.listen();
+});
 
+beforeEach(async () => {
   // we do this instead of passing a fetchFn every time
-  global.fetch = fetch;
+  globalThis.fetch = fetch;
 });
 
 // Reset any request handlers that we may add during the tests,
