eb6c5ef4abb1956c785e3d872a434cfd64bb0acb
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-01-07 02:56:53 +0200

    chore: migrate from yarn to pnpm & support cdn output & update CI

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig((options) => ({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  dts: true,
-  treeshake: true,
-  minify: !options.watch,
-}));
+export default defineConfig((options) => [
+  {
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    treeshake: true,
+    minify: !options.watch,
+  },
+  {
+    // Configuration for CDNs
+    entry: ['src/index.ts'],
+    format: 'iife', // For CDNs (IIFE format for browsers)
+    globalName: 'quran', // Global variable for browser use
+    outDir: 'dist/umd',
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+    dts: true,
+    treeshake: true,
+    minify: !options.watch,
+  },
+]);
