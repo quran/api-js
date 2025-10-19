7a2502c3624390fa624b2ab79621feecfec27464
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-10-19 10:06:13 +0300

    move docs out of src/

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,5 +1,4 @@
 import type { MDXComponents } from "mdx/types";
-import * as TabsComponents from "fumadocs-ui/components/tabs";
 import * as TypeTableComponents from "fumadocs-ui/components/type-table";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 
@@ -7,7 +6,6 @@ import defaultMdxComponents from "fumadocs-ui/mdx";
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    ...TabsComponents,
     ...TypeTableComponents,
     ...components,
   };
